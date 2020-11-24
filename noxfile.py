5593942d7a51dcd51c923972f79520804a01fb71
betsy <elizabeth.alpert@qut.edu.au>
2020-11-24 16:15:49 +1000

    Enable flake8 and black
    - adds config for flake8 and black
    - adds nox sessions lint and check_formatting

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,19 @@ import nox
 
 
 @nox.session
+def check_formatting(session):
+    session.install('black')
+    session.run('black', '--check', '.')
+    # Uses black config from pyproject.toml
+
+
+@nox.session
+def lint(session):
+    session.install('flake8')
+    session.run('flake8')
+
+
+@nox.session
 def test(session):
     session.install('pytest')
     session.install('.')
